0778806b4b8d9ebb82045b3d5a3f7f1efd69bf7c
kaname88 <nagissa793@gmail.com>
2018-01-11 00:42:48 +0900

    first commit

---
+++ src/scripts/HelloForm.js
@@ -0,0 +1,25 @@
+import HelloSayer from './HelloSayer';
+import React, { Component } from 'react';
+import style from '../styles/helloform.scss';
+
+class HelloForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {name: 'world'};
+    }
+
+    render() {
+        return (<div className="hello-form">
+            <input type="text" onChange={this.onChange.bind(this)} />
+            <HelloSayer name={this.state.name} />
+        </div>);
+    }
+
+    onChange(e) {
+        this.setState({
+            name: e.target.value
+        });
+    }
+}
+
+export default HelloForm;
---
+++ src/scripts/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HelloForm from './HelloForm';
+
+
+ReactDOM.render(<HelloForm />, document.getElementById('app'));
